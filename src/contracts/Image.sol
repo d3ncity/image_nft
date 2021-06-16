2121fe67d98397a795018bff8709297852ea23cd
d3ncity <dennythomas13@gmail.com>
2021-06-16 16:01:02 +0800

    totalSupply for map extraction (it's a hack, check for better way)

--- src/contracts/Image.sol
+++ src/contracts/Image.sol
@@ -9,8 +9,11 @@ contract Image is ERC1155 {
 
     mapping(uint256 => string) _uriOfId;
 
+    //the totalSupply ie the number of unique IDs
+    uint256 totalSupply;
+
     constructor() ERC1155("") {
-        //left empty on purpose
+        totalSupply = 0;
     }
 
     //function to create an NFT - mint and set the uri
@@ -28,17 +31,22 @@ contract Image is ERC1155 {
 
     //add an id to the list to later track whether it exists before creating the NFT
     function addId (uint256 _id) private{
+        totalSupply++;
         _idExistTracker[_id] = true;
     }
 
     // function to change (from empty or existing) the URI of a particular NFT
-    function setIdURI (uint256 _anId, string memory _aUri) private{
+    function setIdURI (uint256 _anId, string memory _aUri) public{
         _uriOfId[_anId] = _aUri;
     }
 
     //returns the uri of the object with the id as input
     function uri(uint256 id) public view virtual override returns (string memory) {
         return _uriOfId[id];
+    }
+
+    function getTotalSupply() public view returns(uint256){
+        return totalSupply;
     }
 
 }
