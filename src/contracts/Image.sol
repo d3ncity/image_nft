c1a8c619b2e244df52e1dfeb61aa32163a8eedc1
d3ncity <dennythomas13@gmail.com>
2021-06-16 10:26:59 +0800

    src Image.sol: updated comments and fixed logic

--- src/contracts/Image.sol
+++ src/contracts/Image.sol
@@ -10,16 +10,15 @@ contract Image is ERC1155 {
     mapping(uint256 => string) _uriOfId;
 
     constructor() ERC1155("") {
-        // _mint(msg.sender, GOLD, 10**18, "");
+        //left empty on purpose
     }
 
     //function to create an NFT - mint and set the uri
     function createNFT(uint256 _id, uint256 _amount, bytes memory _data, string memory _uri) public{
-        // require some lists - maybe id and uri?
+        // require that the id is unique (does not exist already)
         require(!_idExistTracker[_id]);
 
         addId(_id);
-        // add uri to bool list here
 
         setIdURI(_id, _uri);
 
@@ -27,6 +26,7 @@ contract Image is ERC1155 {
         
     }
 
+    //add an id to the list to later track whether it exists before creating the NFT
     function addId (uint256 _id) private{
         _idExistTracker[_id] = true;
     }
@@ -36,8 +36,9 @@ contract Image is ERC1155 {
         _uriOfId[_anId] = _aUri;
     }
 
+    //returns the uri of the object with the id as input
     function uri(uint256 id) public view virtual override returns (string memory) {
-        
+        return _uriOfId[id];
     }
 
 }
